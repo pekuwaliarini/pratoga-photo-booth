3c83cb10d14d0adb2cc48e79faa0a3df8b0cf734
pekuwaliarini <82925025+pekuwaliarini@users.noreply.github.com>
2026-08-25 23:03:22 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Opsional: isi URL Google Apps Script Web App jika ingin salinan foto
 // otomatis masuk ke Google Drive panitia.
 // Jika kosong, aplikasi tetap 100% berfungsi untuk mengambil dan mengunduh foto.
-const UPLOAD_ENDPOINT = "";
+const UPLOAD_ENDPOINT = "https://script.google.com/macros/s/AKfycbwfUEEFY2QV879YLmE8mvPJgJSyMsMELOCTxiTypm61MVFTih59lPcApAx6owvSG8LP/exec";
